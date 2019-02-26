0e697a2adf221145b1103ce967b5aafa108a26a4
Fakrichen <fakrichen@gmail.com>
2019-02-26 17:49:51 +0100

    Update constantblackscholesprocess.cpp

--- project1/constantblackscholesprocess.cpp
+++ project1/constantblackscholesprocess.cpp
@@ -1,4 +1,4 @@
-* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 /*
  Copyright (C) 2003 Ferdinando Ametrano
